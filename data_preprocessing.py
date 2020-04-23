96f5d1b27bf86d1466d8e083690247f33330e798
Viljami Raiski <viljami.raiski@aalto.fi>
2020-04-23 13:50:29 +0300

    Data preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -10,7 +10,7 @@ from scipy import ndimage as ndi
 from skimage import feature
 from skimage import util
 import skimage.io as io
-from torchvision import transforms
+import torch
 
 matplotlib.rcParams['font.size'] = 10
 
@@ -23,15 +23,18 @@ def get_canny_filter(img, gaussian_sigma):
     image = img
     # Check if image is RGB and convert to grayscale
     if image.ndim == 3:
-          image = rgb2gray(image)
+        image = rgb2gray(image)
+        print('Gray')
+        print(image.shape)
 
     edges = feature.canny(image, sigma=gaussian_sigma)
     return edges
 
-def filter_images(urls):
+
+def filter_images(imgs):
     """
     Applies the Canny edge-detection filter to a list of images.
-    
+
     Input:
         - urls      List of urls in a batch
     Output:
@@ -39,48 +42,23 @@ def filter_images(urls):
     """
 
     gaussian_sigma = 3
-    load_pattern = urls
+    filtered = []
+    srcs = []
+    # find max dimensions for padding
+    # height = -1
+    # width = -1
+    # for img in ic:
+    #    height = max(height, img.shape[0])
+    #    width = max(width, img.shape[1])
+    for img in imgs:
+        a = get_canny_filter(img, gaussian_sigma)
+        print('a.shape')
+        print(a.shape)
+        print('--------')
+        filtered.append(torch.tensor(get_canny_filter(img, gaussian_sigma)))
 
-    try:
-        
-        filtered = []
-        srcs = []
+    return filtered, imgs
 
-        # find max dimensions for padding
-        #height = -1
-        #width = -1
-        #for img in ic:
-        #    height = max(height, img.shape[0])
-        #    width = max(width, img.shape[1])
-
-        for img in ic:
-            
-            img = transform(img)
-
-            # image dimensions
-            #w = img.shape[1]
-            #h = img.shape[0]
-            #c = img.shape[2]
-
-            ## padding size
-            #padding_h = int((height - h) / 2)
-            #padding_w = int((width - w) / 2)
-
-            ## init padding matrix with c channels
-            #padding = np.zeros((height, width, c))
-
-            ## insert image to padding matrix
-            #padding[padding_h:(padding_h + h):,padding_w:(padding_w + w),:] = img
-            #img = padding
-
-            srcs.append(img)
-            filtered.append(get_canny_filter(img, gaussian_sigma))
-            
-    except Exception as e:
-        print(e)
-        return [], []
-    
-    return filtered, srcs
 
 def plot_paired(edges):
     """
@@ -89,7 +67,7 @@ def plot_paired(edges):
 
     for original, edge in edges:
         fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=1, ncols=4, figsize=(15, 14),
-                                        sharex=True, sharey=True)
+                                                 sharex=True, sharey=True)
         if edge.ndim == 2:
             ax1.imshow(original, cmap=plt.cm.gray)
             ax1.axis('off')
@@ -97,11 +75,11 @@ def plot_paired(edges):
             ax1.imshow(edge)
             ax1.axis('off')
         ax1.set_title('Original')
-        
+
         ax2.imshow(edge, cmap=plt.cm.gray)
         ax2.axis('off')
 
         plt.show()
 
-#edges = filter_images(urls)
-#plot_paired(edges)
+# edges = filter_images(urls)
+# plot_paired(edges)
--- data_set.py
+++ data_set.py
@@ -4,6 +4,8 @@ import pandas as pd
 import data_preprocessing as dp
 import skimage.io as io
 from torchvision import transforms
+import matplotlib.pyplot as plt
+
 
 class EdgeDataset(torch.utils.data.Dataset):
     def __init__(self, filename, transform):
@@ -16,35 +18,52 @@ class EdgeDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         # get one url by idx
         load_pattern = self.urls[idx]
-        ic = torch.tensor(io.imread(load_pattern))
-        
-        ic = self.transform(ic)
-        print(ic.shape)
-        return ic
+        img = torch.zeros((512, 512, 3))
+        try:
+            # print(ic)
+            img = torch.tensor(io.imread(load_pattern))
+            print('Imread')
+            print(img.shape)
+            img = self.transform(img)
+            print('Succesfull')
+
+        except Exception as e:
+            print(e)
+            print('Error')
+        return img
 
     def __len__(self):
         # return length of whole data set (number of rows)
         return len(self.urls)
 
-def collate_fn(urls):
-    sources, targets = dp.filter_images(urls)
-    sources = torch.tensor(sources)
-    targets = torch.tensor(targets)
+
+def collate_fn(imgs):
+    sources, targets = dp.filter_images(imgs)
+    # print(sources[0].shape)
+    print(targets[0].shape)
+    sources = torch.cat(sources)
+    targets = torch.cat(targets)
 
     print("sources", sources.shape)
     print("targets", targets.shape)
     return sources, targets
 
+
 # Uncomment and run 'python data_set.py' to test collate fn:
 width = 512
 height = 512
 transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.RandomCrop((height, width)),
-            transforms.ToTensor()
-        ])
-ds = EdgeDataset('./db/captioned_urls.csv', transform = transform)
-dataloader = torch.utils.data.DataLoader(dataset=ds, batch_size=2, collate_fn=collate_fn, pin_memory=True)
+    transforms.ToPILImage(),
+    transforms.RandomCrop(
+        (height, width), pad_if_needed=True, padding_mode='constant'),
+    transforms.ToTensor()
+])
+ds = EdgeDataset('./db/captioned_urls.csv', transform=transform)
+dataloader = torch.utils.data.DataLoader(
+    dataset=ds, batch_size=2, collate_fn=collate_fn, pin_memory=True)
 
 for src, target in dataloader:
+    break
+    print(src)
+    print(target)
     print("loop")
