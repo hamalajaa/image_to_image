4a118d453289c306afb3cf2d840fac449fe974a6
Elias Panula <elias.panula@gmail.com>
2020-05-14 13:59:32 +0300

    Master Separate dummy main from real main; Add custom bird datasets; Parameterize main.ipynb

--- data_set.py
+++ data_set.py
@@ -3,9 +3,12 @@ import torchvision.transforms as transforms
 import pandas as pd
 import data_preprocessing as dp
 import skimage.io as io
+import os
 from torchvision import transforms
 
-
+"""
+BAM dataset, images from URLs
+"""
 class EdgeDataset(torch.utils.data.Dataset):
     def __init__(self, filename, transform):
         # read csv to memory
@@ -37,38 +40,114 @@ class EdgeDataset(torch.utils.data.Dataset):
         # return length of whole data set (number of rows)
         return len(self.urls)
 
+"""
+CUB2011
+"""
+class BirdEdgeDataset(torch.utils.data.Dataset):
+    def __init__(self, filename, transform):
+        # read images to memory as np arrays
+        file_spec = '*.jpg'
+        file_path = os.path.join(filename, file_spec)
+        self.images = io.imread_collection(file_path)
+        self.transform = transform
+
+
+    def __getitem__(self, idx):
+        # get one image as np array
+        image = self.images[idx]
+        image = self.transform(image)
+        # C * W * H -> W * H * C
+        image = image.permute(1,2,0)
+        return image
+
+    def __len__(self):
+        # return length of whole data set (number of rows)
+        return len(self.images)
+
 
 def collate_fn(imgs):
+    """
+    Outputs sources and targets of shape [batch_size, n_channels, width, height]
+    """
     sources, targets = dp.filter_images(imgs)
     
     sources = torch.stack(sources)
     targets = torch.stack(targets)
+    #print(sources.shape)
+    #print(targets.shape)
+    targets = targets.repeat(1, 1, 1, 3)
+    targets = targets.permute(0, 3, 1, 2)
     
-    targets = targets.permute(0, 3, 1,2)
     sources = sources.unsqueeze(3).repeat(1, 1, 1, 3)
-    sources = sources.permute(0, 3, 1,2).type(torch.FloatTensor)
-
-    print(sources)
-    print(sources.shape)
-    print(targets.shape)
+    #sources = sources.permute(0, 3, 1, 2).type(torch.FloatTensor)
+    sources = sources.permute(0, 3, 1, 2).type(torch.FloatTensor)
+    #print(sources)
+    #print(sources.shape)
+    #print(targets.shape)
     
     return sources, targets
 
+class BirdBWDataset(torch.utils.data.Dataset):
+    def __init__(self, filename, transform):
+        # read images to memory as np arrays
+        file_spec = '*.jpg'
+        file_path = os.path.join(filename, file_spec)
+        self.images = io.imread_collection(file_path)
+        self.transform = transform
+        self.bw_transform = transforms.Grayscale(num_output_channels=1)
+
+
+    def __getitem__(self, idx):
+        # get one image as np array
+        image = self.images[idx]
+        image = self.transform(image)
+        
+        # Create input image (target as black and white)
+        bw_image = self.bw_transform(image)
+        
+        to_tensor = transforms.ToTensor()
+        
+        image = to_tensor(image)
+
+        # C * W * H -> W * H * C
+        #image = image.permute(1, 2, 0)
+        
+        bw_image = to_tensor(bw_image)
+
+        # transofrm to C * W * H -> W * H * C
+        # and repeat the only channel to match input
+        #bw_image = bw_image.permute(1, 2, 0)
+        bw_image = bw_image.repeat(3, 1, 1)
+        
+        # image: target
+        # bw_image: input image
+        return bw_image, image
+
+    def __len__(self):
+        # return length of whole data set (number of rows)
+        return len(self.images)
+
+
 
 # Uncomment and run 'python data_set.py' to test collate fn:
-#width = 512
-#height = 512
-#transform = transforms.Compose([
-#    transforms.ToPILImage(),
-#    transforms.RandomCrop(
-#        (height, width), pad_if_needed=True, padding_mode='constant'),
-#    transforms.ToTensor()
-#])
-#ds = EdgeDataset('./db/captioned_urls.csv', transform=transform)
+width = 512
+height = 512
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomCrop(
+        (height, width), pad_if_needed=True, padding_mode='constant')
+    #transforms.Grayscale(num_output_channels=1),
+    #transforms.ToTensor()
+])
+#ds = BirdEdgeDataset('./db/birds/dummy/', transform=transform)
 #dataloader = torch.utils.data.DataLoader(
 #    dataset=ds, batch_size=2, collate_fn=collate_fn, pin_memory=True)
-#
-#for src, target in dataloader:
-#
-#    print("loop")
-    
+
+ds = BirdBWDataset('./db/birds/dummy/', transform=transform)
+dataloader = torch.utils.data.DataLoader(
+    dataset=ds, batch_size=2, pin_memory=True)
+
+for src, target in dataloader:
+    #print("data set testing loop", src.sum(), target.sum())
+    print(src.shape, target.shape)
+    break    #
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-def init_weights(m, scale_factor=0.2):
+def init_weights(m, scale_factor=0.02):
     if type(m) in [nn.Linear, nn.Conv2d, nn.ConvTranspose2d]:
         nn.init.normal_(m.weight.data, 0.0, scale_factor)
         if hasattr(m, 'bias') and m.bias is not None:
